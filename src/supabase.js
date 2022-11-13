72410f889d01a304ba674378c0cbcd526998d59a
code <code.sengel@gmail.com>
2022-11-13 01:25:22 +0100

    login fixed 5

--- src/supabase.js
+++ src/supabase.js
@@ -3,8 +3,8 @@ import useAuthUser from './composables/UseAuthUser'
 
 
 
-const supabaseUrl = process.env.VUE_APP_SUPA_URL
-const supabaseAnonKey = process.env.VUE_APP_SUPA_ANON
+const supabaseUrl = "https://vzhiunkcjujoxgzlqoyc.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ6aGl1bmtjanVqb3hnemxxb3ljIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQ1MzgzMjksImV4cCI6MTk2MDExNDMyOX0.2EHA5LaMKFK-NjnN4RtYH1voe5DeNyPhLhnEXmMRtTk"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
